034c226d7446cae45f158800219bfb6008845bae
Karl Broman <kbroman@gmail.com>
2019-05-10 16:12:37 -0500

    Example of a bad spreadsheet

--- R/spreadsheets.R
+++ R/spreadsheets.R
@@ -3,6 +3,20 @@
 library(broman)
 
 
+# bad spreadsheet
+pdf("../Figs/bad_spreadsheet.pdf", height=5, width=10)
+mat3 <- rbind(c("1MIN", rep("", 6)),
+              c("", "", "Normal", "", "", "Mutant", ""),
+              c("B6", 146.6, 138.6, 155.6, 166.0, 179.3, 186.9),
+              c("BTBR", 245.7, 240.0, 243.1, 177.8, 171.6, 188.1),
+              rep("", 7),
+              c("5MIN", rep("", 6)),
+              c("", "", "Normal", "", "", "Mutant", ""),
+              c("B6", 333.6, 353.6, 408.8, 450.6, 474.4, 423.8),
+              c("BTBR", 514.4, 610.6, 597.9, 412.1, 447.4, 446.5))
+
+excel_fig(mat3, col_names=FALSE)
+dev.off()
 
 
 
